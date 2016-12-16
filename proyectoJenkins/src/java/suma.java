f54b690752d73310385470eb0977ef88c05f87df
Rafael García <Rafael Garca>
2016-12-16 13:07:57 +0100

    Subida del proyecto

---
+++ proyectoJenkins/src/java/suma.java
@@ -0,0 +1,8 @@
+package java;
+
+public class suma {
+
+	public Double suma(double a, double b){
+		return a+b;
+	}
+}
---
+++ proyectoJenkins/src/test/sumaTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.suma;
+
+import org.junit.Test;
+
+public class sumaTest {
+
+	@Test
+	public void testSuma(){
+		suma sum = new suma();
+		double result = sum.suma(10.2, 50.3);
+		System.out.println(result);
+	}
+}
